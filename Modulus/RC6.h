1c969667be20fb6387106b7cd82b394025ed1ceb
pavellystsov <67075908+pavellystsov@users.noreply.github.com>
2020-06-18 18:07:03 +0300

    Create RC6.h

---
+++ Modulus/RC6.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <fstream>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#include <cryptopp/pwdbased.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/rc6.h>
+
+using namespace std;
+using namespace CryptoPP;
+
+class RC6_Cryptor
+{
+private:
+string FileIn;
+string FileOut;
+string psw;
+
+string salt = "eridogjfv3iwu4oiejfni3co4wiefnjbvieoiwejfnveiorsiefjnveirosiefjnverisgd";
+public:
+RC6_Cryptor() = delete;
+RC6_Cryptor(const string& Input, const string& Output, const string& Pass);
+bool RC6_Encrypt ();
+bool RC6_Decrypt ();
+};
