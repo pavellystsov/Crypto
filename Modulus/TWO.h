c7c899ec5c41c74fc441653e26026f4fbd246868
pavellystsov <67075908+pavellystsov@users.noreply.github.com>
2020-06-18 18:08:55 +0300

    Create TWO.h

---
+++ Modulus/TWO.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <fstream>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#include <cryptopp/pwdbased.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/twofish.h>
+
+using namespace std;
+using namespace CryptoPP;
+
+class Twofish_Cryptor
+{
+private:
+string FileIn;
+string FileOut;
+string psw;
+
+string salt = "eridogjfv3iwu4oiejfni3co4wiefnjbvieoiwejfnveiorsiefjnveirosiefjnverisgd";
+public:
+Twofish_Cryptor() = delete;
+Twofish_Cryptor(const string& Input, const string& Output, const string& Pass);
+bool Twofish_Encrypt ();
+bool Twofish_Decrypt ();
+};
